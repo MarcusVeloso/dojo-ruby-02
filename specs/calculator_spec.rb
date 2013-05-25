47d80865ceaeb19322990055a95505ac3a70acb6
Francisco Martins <franciscomxs@gmail.com>
2013-05-25 20:16:15 +0000

    bugfix

--- specs/calculator_spec.rb
+++ specs/calculator_spec.rb
@@ -3,7 +3,7 @@ require 'calculator'
 describe Calculator do
 	describe '#sum' do
 		it 'deve adicionar 50% a cada soma.' do
-			Calculator c = Calculator.new
+			c = Calculator.new
 			c.sum(1, 1).should == 3
 		end
 	end	
