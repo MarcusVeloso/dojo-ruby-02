495b91b9f6d386eb48cfbe20ac2fab52fe9149d9
Francisco Martins <franciscomxs@gmail.com>
2013-05-25 19:54:25 +0000

    initial commit

---
+++ specs/calculator_spec.rb
@@ -0,0 +1,10 @@
+require 'calculator'
+
+describe Calculator do
+	describe '#sum' do
+		it 'deve adicionar 50% a cada soma.' do
+			Calculator c = Calculator.new
+			c.sum(1, 1).should == 3
+		end
+	end	
+end
